34f1f4f8ec4234d8790bb3888342afcb908da68e
Dilip Sarker <dilipsarker719@gmail.com>
2024-05-30 23:05:09 +0600

    changes saved

--- practice-1.js
+++ practice-1.js
@@ -1,2 +1,5 @@
-let numbers = [4, 6, 3, 5, 8, 10];
-console.log(numbers[numbers.length - 1]);
+const waitForMe = async (request, response) => {
+	return "Don't worry I am waiting for you";
+};
+const herResponse = await waitForMe();
+console.log(herResponse);
--- practice.js
+++ practice.js
@@ -1,9 +1,43 @@
-console.log(1);
-console.log(2);
-setTimeout(doSomething, 3000);
-console.log(4);
-console.log(5);
-
-function doSomething() {
-	console.log(3);
+class Store {
+	constructor(name) {
+		this.name = name;
+		this.items = [];
+		this.prices = {};
+	}
+	isItemAvailable(name) {
+		const itemIndex = this.items.indexOf(name);
+		if (itemIndex == -1) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+	getPrice(name) {
+		const isAvailable = this.isItemAvailable(name);
+		if (isAvailable == true) {
+			const itemPrice = this.prices[name];
+			return itemPrice;
+		} else {
+			console.log("We don't have the item");
+		}
+	}
+	addItem(name, price) {
+		this.items.push(name);
+		this.prices[name] = price;
+	}
 }
+const myStore = new Store("Dilip's Store");
+myStore.addItem('shirt', 500);
+myStore.addItem('pant', 700);
+console.log(myStore.getPrice('shirt'));
+console.log(myStore.getPrice('trouser'));
+
+// console.log(1);
+// console.log(2);
+// setTimeout(doSomething, 3000);
+// console.log(4);
+// console.log(5);
+
+// function doSomething() {
+// 	console.log(3);
+// }
